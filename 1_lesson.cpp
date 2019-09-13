9df8de52a9a2dac227637dfe6664b2dbaf846377
Vlad <53820373+catprogrammer-cat@users.noreply.github.com>
2019-09-14 00:40:42 +0300

    change type variable (float to double)O

--- 1_lesson.cpp
+++ 1_lesson.cpp
@@ -9,15 +9,15 @@ using namespace std;
 
   int main()
 {
-	const float pi = 3.14159265359;
-	const float e = 2.71828182845;
+	const double pi = 3.14159265359;
+	const double e = 2.71828182845;
 
-	float y_1, j_1; //varibles for mathematical formula น1 
-	float z_2, x_2, a_2; //varibles for mathematical formula น2 
-	float x_3; //varibles for mathematical formula น3 
-	float x_4 = 11, a_4, b_4; //varibles for mathematical formula น4 
-	float x_5 = 11; //varibles for mathematical formula น4 
-	float x_6 = 11, p_6 = 2.6; //varibles for mathematical formula น4 
+	double y_1, j_1; //varibles for mathematical formula ยน1 
+	double z_2, x_2, a_2; //varibles for mathematical formula ยน2 
+	double x_3; //varibles for mathematical formula ยน3 
+	double x_4 = 11, a_4, b_4; //varibles for mathematical formula ยน4 
+	double x_5 = 11; //varibles for mathematical formula ยน4 
+	double x_6 = 11, p_6 = 2.6; //varibles for mathematical formula ยน4 
 
 	int choose_formula; //input choose value (angle or radian)
 
@@ -36,7 +36,7 @@ using namespace std;
 			cin >> y_1;
 			cout << "enter varible j\n";
 			cin >> j_1;
-			float result_formula_1_f = (2. * sin(0.354 * y_1 + 1.)) / (log10(y_1 + 2. * j_1));//formula #1
+			double result_formula_1_f = (2. * sin(0.354 * y_1 + 1.)) / (log10(y_1 + 2. * j_1));//formula #1
 			cout << "result: " << result_formula_1_f;
 			break;
 		}
@@ -49,7 +49,7 @@ using namespace std;
 			cin >> x_2;
 			cout << "enter varible a\n";
 			cin >> a_2;
-			float result_formula_2_n = (pow((z_2 + pow(z_2 * x_2, 1. / 2)), (1. / 5))) / (pow(e, x_2) + pow(a_2, 5) * atan(x_2));//formula #2
+			double result_formula_2_n = (pow((z_2 + pow(z_2 * x_2, 1. / 2)), (1. / 5))) / (pow(e, x_2) + pow(a_2, 5) * atan(x_2));//formula #2
 			cout << "result: " << result_formula_2_n;
 			break;
 		}
@@ -58,7 +58,7 @@ using namespace std;
 			cout << "*\n" << "*\n" << "  3.y = (pow(x,2)+2*sin(x) ) / (2*x+1) + (pow(x,1/2) - cos(x) / ((2*x+1)) * (log10((pow(x, 2) + 1))))\n" << "*\n";
 			cout << "enter varible x\n";
 			cin >> x_3;
-			float result_formula_3_y = (pow(x_3, 2.) + 2. * sin(x_3)) / (2. * x_3 + 1.) + (pow(x_3, 1. / 2.) - cos(x_3) / ((2. * x_3 + 1)) * (log10((pow(x_3, 2.) + 1.))));//formula #3
+			double result_formula_3_y = (pow(x_3, 2.) + 2. * sin(x_3)) / (2. * x_3 + 1.) + (pow(x_3, 1. / 2.) - cos(x_3) / ((2. * x_3 + 1)) * (log10((pow(x_3, 2.) + 1.))));//formula #3
 			cout << "result: " << result_formula_3_y;
 			break;
 		}
@@ -72,7 +72,7 @@ using namespace std;
 			cin >> a_4;
 			cout << "enter varible b\n";
 			cin >> b_4;
-			float result_formula_4_y = pow(x_4, 1. / 2) * sin(a_4) + pow(b_4, 1. / 2) * cos(x_4);//formula #4
+			double result_formula_4_y = pow(x_4, 1. / 2) * sin(a_4) + pow(b_4, 1. / 2) * cos(x_4);//formula #4
 			cout << "result: " << result_formula_4_y;
 			break;
 		}
@@ -82,7 +82,7 @@ using namespace std;
 			// x = 11;
 			// p = 2.6;
 			cout << "the x is constant;\n x = 11\n";
-			float result_formula_5_a = log10(abs(x_5));//formula #5
+			double result_formula_5_a = log10(abs(x_5));//formula #5
 			cout << "result: " << result_formula_5_a;
 			break;
 		}
@@ -93,7 +93,7 @@ using namespace std;
 			// p = 2.6;
 			cout << "the x is constant;\n x = 11\n";
 			cout << "the p is constant;\n p = 2.6\n";
-			float result_formula_6_b = x_6 + pow(p_6, 3);//formula #6
+			double result_formula_6_b = x_6 + pow(p_6, 3);//formula #6
 			cout << "result: " << result_formula_6_b;
 			break;
 		}
